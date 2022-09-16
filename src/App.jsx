2d6b3f3adc29eec393fcca86aabf5abb919bb7ec
Amelia Baldwin <amelia01000010@gmail.com>
2022-09-16 14:54:21 +0100

    router change

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import NavBar from './components/NavBar/NavBar';
 import BeerInfo from './components/Main/BeerInfo/BeerInfo';
 import Button from './components/NavBar/Button/Button';
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { useState,useEffect } from "react";
 import { Link } from 'react-router-dom';
 
